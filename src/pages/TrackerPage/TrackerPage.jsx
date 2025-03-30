cd4139da1d596d1eeeb613f03b3416c538f392fa
KoptevAnton <koptev110593@gmail.com>
2025-03-30 20:44:41 +0200

    fix: date selection when switching months and add month translations

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,23 +1,23 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { selectSelectedDate } from '../../redux/water/selectors.js';
+import { selectSelectedMonth } from '../../redux/water/selectors.js';
 import { useEffect, useState } from 'react';
-import WaterDetailedInfo from '../../components/WaterDetailedInfo/WaterDetailedInfo';
-import WaterMainInfo from '../../components/WaterMainInfo/WaterMainInfo';
-import css from './TrackerPage.module.css';
-import clsx from 'clsx';
 import {
   getWaterByMonth,
   getWaterRecords,
 } from '../../redux/water/operations.js';
-import TourSteps from '../../onboardingTour/TourSteps.jsx';
 import dayjs from 'dayjs';
+import WaterDetailedInfo from '../../components/WaterDetailedInfo/WaterDetailedInfo';
+import WaterMainInfo from '../../components/WaterMainInfo/WaterMainInfo';
+import TourSteps from '../../onboardingTour/TourSteps.jsx';
+import clsx from 'clsx';
+import css from './TrackerPage.module.css';
 
 export default function TrackerPage() {
   const dispatch = useDispatch();
   const [isTour, setIsTour] = useState(false);
-  const selectedDate = useSelector(selectSelectedDate);
-  const selectedDateFormatted = selectedDate.split('T')[0];
-  const [year, month] = selectedDateFormatted.split('-');
+  const selectedMonth = useSelector(selectSelectedMonth);
+
+  const [year, month] = selectedMonth.split('-');
   const todayDate = dayjs().format('YYYY-MM-DD');
 
   useEffect(() => {
